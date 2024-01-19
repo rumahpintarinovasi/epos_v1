4cebc24917510b4bfbc39b54354b39a5cf902a15
Gerry S Sidharta <138549811+hackermu2001@users.noreply.github.com>
2024-01-19 09:31:05 +0800

    fix : path wkhtmltopdf

--- config/snappy.php
+++ config/snappy.php
@@ -36,7 +36,7 @@ return [
     'pdf' => [
         'enabled' => true,
         //'binary'  => env('WKHTML_PDF_BINARY', base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),),
-        'binary'  => env('WKHTML_PDF_BINARY', 'C:\\wkhtmltopdf\\bin\\wkhtmltopdf.exe'),
+        'binary'  => env('WKHTML_PDF_BINARY', '/usr/bin/wkhtmltopdf'),
         'timeout' => false,
         'options' => [
             'enable-local-file-access' => true,
